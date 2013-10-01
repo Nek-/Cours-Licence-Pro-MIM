de7c048f4e4c5a3657d45e3ec6c383b4756777f2
OpenShift guest <nek.dev@gmail.com>
2013-10-01 09:23:25 -0400

    Ajoute le fichier rect.js qui montre comment faire des rectangles avec canvasjs

---
+++ CanvasJS/rect.js
@@ -0,0 +1,9 @@
+var c   = document.getElementById('canevasMIM');
+var ctx = c.getContext('2d');
+
+
+ctx.strokeStyle = 'blue';
+ctx.fillStyle   = 'red';
+
+ctx.fillRect(10, 10, 150, 90);
+ctx.strokeRect(10, 10, 150, 90);
