23dc3850c0cae3ff3ceb2c82b7e90a2208eb4682
OpenShift guest <nek.dev@gmail.com>
2013-10-04 03:00:59 -0400

    Super spirale de la mort

--- CanvasJS/image.js
+++ CanvasJS/image.js
@@ -10,6 +10,7 @@ img.onload = function() {
     }
     */
     
+    /*
     var pas = Math.PI * 2 / 10;
     // i de 0 à 2*PI
     // Centre du cercle: 100, 100
@@ -18,6 +19,20 @@ img.onload = function() {
     for (var i = 0; i <= 2 * Math.PI; i = i + pas) {
         ctx.drawImage(img, 100+100*Math.cos(i), 100+100*Math.sin(i));
     }
+    */
+    var pas = Math.PI * 2 / 10;
+    // Spirale de ouf tavu
+    for (var i = 0; i <= 2 * Math.PI; i = i + pas) {
+        ctx.drawImage(img, 100+i*30*Math.cos(i), 100+i*30*Math.sin(i));
+    }
+    
+    /*
+    // Lemniscate de bernouicci
+    var a = 20;
+    for (var t = 0; t < 2*Math.PI; t += 0.5) {
+        ctx.drawImage(img, 100+100*Math.cos(i), 100+100*Math.sin(i));
+    }
+    */
 }
 
 img.src = 'images/coquelicot.png';
