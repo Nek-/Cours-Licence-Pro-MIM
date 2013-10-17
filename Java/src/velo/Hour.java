48a495ebe7cfbb3fdd7420a75776f489696d3cf0
OpenShift guest <nek.dev@gmail.com>
2013-10-17 05:56:20 -0400

    Ajoute le début de la classe "Hour"

--- Java/src/velo/Hour.java
+++ Java/src/velo/Hour.java
@@ -1,5 +1,28 @@
 package velo;
 
+/**
+ * Classe permettant de simplifier le traitement
+ * des heures
+ */
 class Hour {
-    public static Hour createHourFromString(String );
+    
+    private int hour;
+    private int minute;
+    
+    /**
+     * Méthode statique qui permet d'instancier
+     * une "Hour" à l'aide d'une chaîne de caractères
+     * 
+     * @param String hour Chaîne de caractères sous la forme "8h15"
+     */
+    public static Hour createHourFromString(String hour)
+    {
+        
+    }
+    
+    public Hour (int hour, int minute)
+    {
+        this.hour   = hour;
+        this.minute = minute;
+    }
 }
