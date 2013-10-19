d4eb14106dd4ad3c9cf29aab29ba2915c8a93fb9
OpenShift guest <nek.dev@gmail.com>
2013-10-19 18:35:57 -0400

    Ajoute la partie calcul de la classe "Bike"

--- Java/src/velo/Bike.java
+++ Java/src/velo/Bike.java
@@ -2,6 +2,9 @@ package velo;
 
 import java.util.Scanner;
 
+/**
+ * Exemple d'implémentation (simplifiée) de calcul de paiement pour le vélo'v
+ */
 class Bike {
     
     private Hour start;
@@ -15,11 +18,16 @@ class Bike {
     public static void main(String[] args)
     {
         Bike v = new Bike();
+        
+        System.out.println("Vous devez payer: " + String.valueOf(v.calculate()) + " euros" );
     }
     
     /**
      * Construit la classe vélo
      * en demandant les informations à l'utilisateur
+     * 
+     * Note: ici, aucune véfication n'est faite par rapport à ce que l'utilisateur
+     * entre, ça serait une bonne amélioration que de vérifier les données
      */
     public Bike()
     {
@@ -37,6 +45,52 @@ class Bike {
         // Note: le programme va planter si l'utilisateur ne rentre pas un nombre
         System.out.println("Entrez votre âge :");
         this.age   = Scanner.nextInt();
+    }
+    
+    /**
+     * Calcule le prix que va payer l'utilisateur
+     */'
+    public float calculate()
+    {
+        // float représente un nombre à virgule
+        float res = 0.0;
+        
+        if (this.start.before(new Hour(7,0)) {
+            
+            // Note: ne pas utiliser 01, cela serait compris comme un nombre "octal"
+            // et pas "décimal" (ce qui n'est pas grave de 1 à 7)
+            if (this.end.before(new Hour(7,1)) {
+                // diff retourne un entier, pour préciser que le résultat doit
+                // être un nombre à virgule on utilise des nombres à virgules
+                // dans l'opération
+                res = this.start.diff(this.end) / 60.0 * 1.25;
+            } else {
+                res += this.start.diff(new Hour(7,0)) / 60.0 * 1.25;
+            }
+        }
+        
+        if (this.end.before(new Hour(17,0))) {
+            if (this.start.after(new Hour(7,0))) {
+                res = this.start.diff(this.end) / 60.0 * 1.5;
+            } else {
+                res += this.end.diff(new Hour(7,0) / 60.0 * 1.5;
+            }
+        }
+        
+        if (this.end.after(new Hour(17,0))) {
+            if (this.start.after(new Hour(17,0))) {
+                res = this.start.diff(this.end) / 60.0 * 1.25;
+            } else {
+                res += this.start.diff(new Hour(17,0)) / 60.0 * 1.25;
+            }
+        }
+        
+        // La promotion pour les jeunes:
+        if (this.age < 30 || this.age > 75) {
+            res = res * 0.8;
+        }
+        
+        return res;
     }
 }
 
--- Java/src/velo/Hour.java
+++ Java/src/velo/Hour.java
@@ -6,7 +6,16 @@ package velo;
  */
 class Hour {
     
+    /**
+     * L'heure (un nombre entier)
+     * @var int hour
+     */
     private int hour;
+    
+    /**
+     * Les minutes (un nombre entier)
+     * @var int minute
+     */
     private int minute;
     
     /**
@@ -17,12 +26,68 @@ class Hour {
      */
     public static Hour createHourFromString(String hour)
     {
+        // On coupe la chaîne de caractères en deux dans un tableau
+        // de chaînes de caractères.
+        // On utilise la méthode split dont la doc est disponible ici:
+        // http://docs.oracle.com/javase/7/docs/api/java/lang/String.html#split%28java.lang.String%29
+        String[] hourSplitted = hour.split("h");
         
+        // La méthode static Integer.parseInt() permet de transformer les
+        // chaînes de caractères en nombres
+        return new Hour(
+            hourSplitted[0],
+            hourSplitted[1]
+        );
     }
     
-    public Hour (int hour, int minute)
+    /**
+     * Le constructeur de la classe
+     * il initialise les variables contenues
+     * 
+     * @param int hour Un nombre représentant l'heure
+     * @param int minute Un nombre représentant les minutes
+     */
+    public Hour(int hour, int minute)
     {
         this.hour   = hour;
         this.minute = minute;
+    }
+    
+    /**
+     * Calcule la différence entre l'heure courante et l'heure passée
+     * en paramètres
+     * 
+     * @param Hour otherHour
+     * @return int toujours positif représentant la différence entre les deux heures
+     */
+    public int diff(Hour otherHour)
+    {
+        // TODO
+        return 0;
+    }
+    
+    /**
+     * Retourne "vrai" si l'heure précède l'heure donnée en paramètres
+     * 
+     * @param Hour otherHour
+     * @return boolean
+     */
+    public boolean before(Hour otherHour)
+    {
+        // TODO
+        return false;
+    }
+    
+    
+    /**
+     * Retourne "vrai" si l'heure suit l'heure donnée en paramètres
+     * 
+     * @param Hour otherHour
+     * @return boolean
+     */
+    public boolean after(Hour otherHour)
+    {
+        // TODO
+        return false;
     }
 }
